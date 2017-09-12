491453bb1d3d53fa8a976c8dc2d9340681fe385e
Mauricio Acevedo <mauricio.acevedo@ariadnacg.com>
2017-09-12 17:22:18 -0500

    Video 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App= () => <h1>hi</h1>
+class App extends React.Component{
+  render(){
+    return <h1>Hello</h1>
+    //return React.createElement('h1',null,'Hello return')
+  }
+}
+
+//const App = () => <h1>hello consts</h1>
 
 export default App
